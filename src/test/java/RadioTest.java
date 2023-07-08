e51dea8e81a099d4e558bf9591d3e655b250c16f
Serega219 <sergeyfns15@gmail.com>
2023-07-08 12:48:43 +0300

    new2

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Radiotest {
+public class RadioTest {
     @Test
     public void testCurrentStantion2() {
         Radio radio = new Radio();
